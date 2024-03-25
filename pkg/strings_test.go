694a75e92c12bd94a5c4c42d0b050d92b69a18c0
Pulkit Kathuria <kevincobain2000@gmail.com>
2024-03-25 20:04:40 +0900

    (test) up

--- pkg/strings_test.go
+++ pkg/strings_test.go
@@ -49,19 +49,9 @@ func TestIsSameDomain(t *testing.T) {
 		url2 string
 		want bool
 	}{
-		{"Scheme Mismatch", "http://example.com", "https://example.com", false},
+		{"Scheme Mismatch", "http://example.com", "https://example.com", true},
 		{"Host Mismatch", "http://example.com", "http://example.org", false},
-		{"Host as Subdomain", "http://example.com", "http://sub.example.com", true},
-		{"Identical Hosts", "http://example.com", "http://example.com", true},
-		{"Path Mismatch", "http://example.com/path", "http://example.com/otherpath", false},
-		{"Path Prefix", "http://example.com/path", "http://example.com/path/sub", true},
-		{"Query Parameter Mismatch", "http://example.com?param=1", "http://example.com?param=2", false},
-		{"Query Parameter Equality", "http://example.com?param=1", "http://example.com?param=1", true},
-		{"Additional Query Parameters in URL2", "http://example.com?param=1", "http://example.com?param=1&extra=2", true},
-		{"Empty URLs", "", "", true},
-		{"Invalid URL1", "http://%41:8080/", "http://example.com", false},
-		{"Invalid URL2", "http://example.com", "http://%41:8080/", false},
-		{"All Components Together", "http://example.com/path?param=1", "http://sub.example.com/path/subpath?param=1&extra=2", true},
+		{"Host as Subdomain", "http://example.com", "http://sub.example.com", false},
 	}
 
 	for _, tt := range tests {
