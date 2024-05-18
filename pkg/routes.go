5baf1f4ced8487e8189914fa1ba98afaa013516a
Pulkit Kathuria <kevincobain2000@gmail.com>
2024-05-18 12:49:23 +0900

    (feat) robots.txt

--- pkg/routes.go
+++ pkg/routes.go
@@ -15,6 +15,7 @@ func SetupRoutes(e *echo.Echo, options []EchoOption) {
 	}
 	e.GET(opt.BaseURL+"", NewAssetsHandler(opt.PublicDir, "index.html").GetHTML)
 	e.GET(opt.BaseURL+"extract", NewAssetsHandler(opt.PublicDir, "extract/index.html").GetHTML)
+	e.GET(opt.BaseURL+"robots.txt", NewAssetsHandler(opt.PublicDir, "robots.txt").GetPlain)
 	e.GET(opt.BaseURL+"favicon.ico", NewAssetsHandler(opt.PublicDir, "favicon.ico").GetIco)
 	e.GET(opt.BaseURL+"api/extract",
 		NewExtractHandler().Get,
